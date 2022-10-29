19ce00d5862d06f2169a98da968700fac60a6d4e
girliehappy <etetimh@gmail.com>
2022-10-29 07:45:51 +0000

    musicapp to be worked on later

--- songcrud/musicapp/models.py
+++ songcrud/musicapp/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class Artiste(models.Model):
+    first_name = models.Charfield(max_length=10)
+    last_name = models.Charfield(max_length=10)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.first_name +' '+ self.last_name
+
+class Song(models.Model):
+    title = models.Charfield(max_length=10)
+    date_released = models.CharField(max_length=10)
+    likes = models.CharField(max_length=10)
+    artiste_id = models.IntegerField()
+    Artiste = models.ForeignKey(Artiste, on_delete=models.CASCADE,)
+
+    def __str__(self):
+        return self.title
+
+class Lyrics(models.Model):
+    content = models.CharField(max_length=10)
+    sond_id = models.IntegerField()
+    song = models.ForeignKey(Song, on_delete=models.CASCADE,)
+
+    def __str__(self):
+        return self.content
